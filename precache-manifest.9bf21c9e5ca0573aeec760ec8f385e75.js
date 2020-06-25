bb8964f8c35bbeb8cdfec33d5db0ff33f1bcf0a0
ghana7989 <ghana.chindukuri@gmail.com>
2020-06-25 18:38:46 +0530

    First Commit

---
+++ precache-manifest.9bf21c9e5ca0573aeec760ec8f385e75.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ead4d1ac0e2624ee368f51f5a19c670",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68029cae79872d730ebf",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "21c8c757b8e111004397",
+    "url": "/static/css/main.2cf75415.chunk.css"
+  },
+  {
+    "revision": "68029cae79872d730ebf",
+    "url": "/static/js/2.d2e9c624.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d2e9c624.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21c8c757b8e111004397",
+    "url": "/static/js/main.598a4fd5.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
